840f473e2052cb82ef6034dec950265128b6ddca
ntunyuserge <ntunyuserge@gmail.com>
2023-02-06 12:29:48 +0100

    Added new file

---
+++ tic-tac-toe/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{color:"brown"}}>Tic-Tac-Toe Game using React</h1>
+    </div>
+  );
+}
+
+export default App;
