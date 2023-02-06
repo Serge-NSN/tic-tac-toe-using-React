19cd933dfe692de5c79354708b3fc82d7957db23
ntunyuserge <ntunyuserge@gmail.com>
2023-02-06 14:26:53 +0100

    Added the game board and general layout

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,9 +1,40 @@
 import './App.css';
 
+function Square({ value }){
+  function handleClick(){
+    console.log('clicked!');
+  }
+  return (
+    <button 
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
+}
 function App() {
   return (
     <div className="App">
       <h1 style={{color:"brown"}}>Tic-Tac-Toe Game using React</h1>
+      <br />
+      <div className="board-row">
+        <Square value="X"/>
+        <Square value="O"/>
+        <Square />
+
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+
     </div>
   );
 }
