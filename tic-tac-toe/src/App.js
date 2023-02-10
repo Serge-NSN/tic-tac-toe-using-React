d2d84e397ce17c703a709ceb533dc3879f4e8008
ntunyuserge <ntunyuserge@gmail.com>
2023-02-10 06:49:32 +0100

    Created array to store states of squrares

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,40 +1,36 @@
 import './App.css';
 import { useState } from 'react';
 
-function Square(){
-  const [value, setValue] = useState(null);
-  function handleClick(){
-    setValue('O');
-  }
+function Square({value, onSquareClick}){
   return (
     <button 
-      className="square"
-      onClick={handleClick}
-    >
+      className="square" onClick={onSquareClick}>
       {value}
     </button>
   );
 }
 function App() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  
   return (
     <div className="App">
       <h1 style={{color:"brown"}}>Tic-Tac-Toe Game using React</h1>
       <br />
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} onSquareClick={handleClick}/>
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
 
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]}/>
+        <Square value={squares[4]}/>
+        <Square value={squares[5]}/>
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]}/>
+        <Square value={squares[7]}/>
+        <Square value={squares[8]}/>
       </div>
 
     </div>
